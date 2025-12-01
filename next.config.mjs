68529ce35f0b3f389690f4977d37795014ac8448
ditcu-code <work.adityac@gmail.com>
2025-12-01 11:41:56 +0700

    chore: Update next.config.mjs to disable poweredByHeader and increase minimumCacheTTL; add sharp dependency in package.json and update yarn.lock for version consistency.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+  poweredByHeader: false,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -8,7 +9,7 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 31536000,
   },
 };
 
