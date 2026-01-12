89539fafe6c7763075775c51764c89d9ca6eab7f
ditcu-code <work.adityac@gmail.com>
2026-01-12 17:55:31 +0700

    Simplify blog list loading skeleton

--- app/[locale]/(main)/blog/(list)/loading.tsx
+++ app/[locale]/(main)/blog/(list)/loading.tsx
@@ -8,24 +8,6 @@ export default function BlogLoading() {
         {/* Page Header */}
         <BlogHeaderSection />
 
-        {/* Featured Hero Loading */}
-        <div className="relative mb-16 w-full md:mb-24">
-          <div className="grid items-center gap-6 md:grid-cols-12">
-            {/* Image Side */}
-            <div className="relative aspect-4/3 overflow-hidden rounded-lg md:col-span-7 md:aspect-video lg:col-span-8">
-              <Skeleton className="h-full w-full" />
-            </div>
-
-            {/* Text Side */}
-            <div className="flex flex-col justify-center space-y-4 md:col-span-5 md:pl-6 lg:col-span-4">
-              <Skeleton className="h-4 w-24" /> {/* Tag */}
-              <Skeleton className="h-12 w-3/4" /> {/* Title */}
-              <Skeleton className="h-24 w-full" /> {/* Excerpt */}
-              <Skeleton className="h-6 w-32" /> {/* Read More */}
-            </div>
-          </div>
-        </div>
-
         {/* Divider */}
         <div className="border-border/40 my-16 border-t" />
 
