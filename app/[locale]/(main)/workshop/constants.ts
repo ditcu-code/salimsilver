d4ebb6dd961a0cbc27964e8e4764097de19ce5fb
ditcu-code <work.adityac@gmail.com>
2026-07-26 20:53:11 +0700

    fix: correct Instagram URL paths from /reels/ to /reel/ in workshop constants

--- app/[locale]/(main)/workshop/constants.ts
+++ app/[locale]/(main)/workshop/constants.ts
@@ -15,7 +15,7 @@ export const reels = [
   "https://www.tiktok.com/@experiencejogja/video/7631175893511179540",
   "https://www.tiktok.com/@experiencejogja/video/7644545194871835924",
   "https://www.tiktok.com/@experiencejogja/video/7630290092782849301",
-  "https://www.instagram.com/reels/DEoZ7RFSORY/",
-  "https://www.instagram.com/reels/DbM0f2HzMt2/",
-  "https://www.instagram.com/reels/DEq2UFBPMZv/"
+  "https://www.instagram.com/reel/DEoZ7RFSORY/",
+  "https://www.instagram.com/reel/DbM0f2HzMt2/",
+  "https://www.instagram.com/reel/DEq2UFBPMZv/"
 ]
